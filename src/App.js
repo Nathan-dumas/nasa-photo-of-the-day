72b73ee8be6794ba2382d409e605c0f131bb7241
NDCT94 <nathan.dumas01@gmail.com>
2020-10-07 17:03:45 -0400

    forgot commits, MVP met I think

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import "./App.css"
+import axios from 'axios'
+import Card from './components/Card'
 
 function App() {
+  
+  const [nasa, setNasa] = useState([])
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        setNasa(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
+    }
+    fetchData()
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div>
+      <Card hdurl={nasa.hdurl} copyright={nasa.copyright} date={nasa.date} title={nasa.title} explanation={nasa.explanation}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function Card(props) {
+    const { copyright, date, explanation, hdurl, title } = props
+
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h2>Copyright: {copyright}</h2>
+            <h2>Taken on: {date}</h2>
+            <img src={hdurl} alt="NASA potd" width="50%" />
+            <p>{explanation}</p>
+        </div>
+    )
+}
